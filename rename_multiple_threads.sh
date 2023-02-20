136af0647d2fbf8ac64e95709c821fc9ef390eeb
estuhr <estuhr@Ubuntu2204.myguest.virtualbox.org>
2023-02-20 12:25:32 -0500

    adding renmaing script to this branch

---
+++ rename_multiple_threads.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get the prefix and postfix arguments
+PREFIX="$1"
+CENTER="$2"
+POSTFIX="$3"
+
+# postfix should include the t, but not the number. ex. sim_name_t, but not sim_name_t12
+# Loop through all directories that match the pattern "${PREFIX}*"
+# example usage: rename directory temp_name_t0 to temp_t0
+# command: ./rename_multiple_threads.sh temp_ name_ t
+i=0
+for DIR in ${PREFIX}${CENTER}${POSTFIX}*
+do
+    # Rename the directory to the new name
+    NEW_NAME="${PREFIX}${POSTFIX}${i}"
+    mv "${DIR}" "${NEW_NAME}"
+    ((i++))
+done
